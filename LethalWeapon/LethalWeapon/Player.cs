09d4290ac15fc7f00201a5df114ac5e00a501b85
sebbeklaren <sebbeklaren@gmail.com>
2017-03-16 14:15:48 +0100

    Ny klass
    Lagt till plyer

---
+++ LethalWeapon/LethalWeapon/Player.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalWeapon
+{
+    class Player : GameObject
+    {
+
+
+        public Player(Texture2D texture, Vector2 position): base (texture, position)
+        {
+
+        }
+    }
+}
