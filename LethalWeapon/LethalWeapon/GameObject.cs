fad16ed669eac74ad0527e8d0542959446fccdeb
Haetora <johzzy@hotmail.se>
2017-03-16 14:18:32 +0100

    Ny Klass
    GameObject

---
+++ LethalWeapon/LethalWeapon/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LethalWeapon
+{
+    class GameObject
+    {
+        public Texture2D texture;
+        public Vector2 position;
+
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, Color.White);
+        }
+    }
+}
