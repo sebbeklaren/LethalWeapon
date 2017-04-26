8e671789c859bf5c28dfa166cd436d7610d6e6de
sebbeklaren <sebbeklaren@gmail.com>
2017-04-26 18:31:31 +0200

    Röda hitboxar för väggar
    Bara för att se så de ritas ut rätt

--- LethalWeapon/LethalWeapon/Tiles.cs
+++ LethalWeapon/LethalWeapon/Tiles.cs
@@ -15,10 +15,15 @@ namespace LethalWeapon
         public bool Wall
         {
             get { return wall; }
+            set { wall = value; }
         }
         public Rectangle SourceRect
         {
             get { return sourceRect; }
+        }
+        public Vector2 Position
+        {
+            get { return position; }
         }
 
         public Tiles(Texture2D texture, Vector2 position, Rectangle sourceRect, bool wall)
